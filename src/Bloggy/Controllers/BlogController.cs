df7ca602fb8dfb645cfac1a669b194b09d77e19c
hishamco <hishamco_2007@yahoo.com>
2016-12-10 01:21:37 +0300

    Home -> Blog

--- src/Bloggy/Controllers/BlogController.cs
+++ src/Bloggy/Controllers/BlogController.cs
@@ -2,7 +2,7 @@
 
 namespace Bloggy.Controllers
 {
-    public class HomeController : Controller
+    public class BlogController : Controller
     {
         [Route("/")]
         public IActionResult Index()
