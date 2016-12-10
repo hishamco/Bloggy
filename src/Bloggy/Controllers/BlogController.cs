0f140185eda0efab4e82337e2ccf3f3ce476dab6
hishamco <hishamco_2007@yahoo.com>
2016-12-10 04:08:01 +0300

    Use DI to load the data from EF InMemory store

--- src/Bloggy/Controllers/BlogController.cs
+++ src/Bloggy/Controllers/BlogController.cs
@@ -1,13 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bloggy.Models;
+using Bloggy.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Bloggy.Controllers
 {
     public class BlogController : Controller
     {
+        private IBlogService _blogService;
+
+        public BlogController(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+
         [Route("/")]
         public IActionResult Index()
         {
-            return View();
+            var posts = GetLatestPosts();
+
+            return View(posts);
+        }
+
+        private IEnumerable<Post> GetLatestPosts(int postsNo = 5)
+        {
+            return _blogService.GetPosts()
+                .Where(p => p.IsPublished)
+                .OrderByDescending(p => p.PublishedAt.Value)
+                .Take(postsNo);
         }
     }
 }
