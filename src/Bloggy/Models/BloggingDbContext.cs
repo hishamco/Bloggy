6a8836762dda872cdb011471f990e72a21e84738
hishamco <hishamco_2007@yahoo.com>
2016-12-10 04:04:51 +0300

    Add models

---
+++ src/Bloggy/Models/BloggingDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Bloggy.Models
+{
+    public class BloggingDbContext : DbContext
+    {
+        public BloggingDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Post> Posts { get; set; }
+    }
+}
---
+++ src/Bloggy/Models/Post.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Bloggy.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Slug { get; set; }
+
+        public string Excerpt { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime LastModified { get; set; }
+
+        public bool IsPublished { get; set; }
+
+        public DateTime? PublishedAt { get; set; }
+
+        public string[] Tags { get; set; }
+    }
+}
