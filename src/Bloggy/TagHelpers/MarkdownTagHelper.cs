59fa63d5629ce563ab8f08a5d8bbd9ba66f9ebc6
hishamco <hishamco_2007@yahoo.com>
2016-12-16 18:46:12 +0300

    Support markdown syntax in both posts & comments

---
+++ src/Bloggy/TagHelpers/MarkdownTagHelper.cs
@@ -0,0 +1,36 @@
+﻿using CommonMark;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace Bloggy.TagHelpers
+{
+    [HtmlTargetElement("markdown", TagStructure = TagStructure.NormalOrSelfClosing)]
+    [HtmlTargetElement(Attributes = "markdown")]
+    public class MarkdownTagHelper : TagHelper
+    {
+        public ModelExpression Content { get; set; }
+
+        public async override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            if (output.TagName == "markdown")
+            {
+                output.TagName = null;
+            }
+            output.Attributes.RemoveAll("markdown");
+
+            var content = await GetContent(output);
+            var markdown = content;
+            var html = CommonMarkConverter.Convert(markdown);
+            output.Content.SetHtmlContent(html ?? string.Empty);
+        }
+
+        private async Task<string> GetContent(TagHelperOutput output)
+        {
+            if (Content == null)
+                return (await output.GetChildContentAsync()).GetContent();
+
+            return Content.Model?.ToString();
+        }
+    }
+}
