b59039859b3cef01aeeefb64c2c71c0041b4bbae
hishamco <hishamco_2007@yahoo.com>
2016-12-19 01:54:07 +0300

    Display alerts on comment submission

---
+++ src/Bloggy/Extensions/ViewContextExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Bloggy.Extensions
+{
+    public static class ViewContextExtensions
+    {
+        public static bool IsPost(this ViewContext viewContext)
+        {
+            return viewContext.HttpContext.Request.Method == "POST";
+        }
+    }
+}
